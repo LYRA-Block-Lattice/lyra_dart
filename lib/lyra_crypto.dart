afd4e1de227da4699c0b645b169ab3eb19a9bb9e
wizard <rcvbuf@gmail.com>
2021-08-12 02:12:24 +0800

    change crypto lib to pointycastle

--- lib/lyra_crypto.dart
+++ lib/lyra_crypto.dart
@@ -8,15 +8,14 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:pascaldart/pascaldart.dart';
 import 'package:convert/convert.dart';
-import 'package:cryptography/cryptography.dart';
+import 'package:pointycastle/ecc/curves/secp256r1.dart';
+import 'package:pointycastle/pointycastle.dart';
 
 /// A Calculator.
 class LyraCrypto {
-  /// Verify a key pair
-  Future<bool> isAccountIdValid(String accountId) async {
-    if (accountId.length < 10 || accountId.substring(0, 1) != 'L') return false;
+  bool isPrivateKeyValid(String privateKey) {
     try {
-      var decStr = await lyraDecAccountId(accountId);
+      var decStr = lyraDec(privateKey);
       return true;
     } catch (e) {
       print(e);
@@ -24,17 +23,28 @@ class LyraCrypto {
     }
   }
 
-  Future<String> lyraDecAccountId(String accountId) async {
+  bool isAccountIdValid(String accountId) {
+    if (accountId.length < 10 || accountId.substring(0, 1) != 'L') return false;
+    try {
+      var decStr = lyraDecAccountId(accountId);
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  String lyraDecAccountId(String accountId) {
     var pubKey = accountId.substring(1);
 
-    var decStr = await lyraDec(pubKey);
+    var decStr = lyraDec(pubKey);
     return "04" + decStr;
   }
 
-  Future<String> lyraDec(String input) async {
+  String lyraDec(String input) {
     var buff = Base58.decode(input);
     var data = buff.sublist(0, buff.length - 4);
-    var crc = await checksum(data);
+    var crc = checksum(data);
     Function eq = const ListEquality().equals;
     print("buff len: " + buff.length.toString());
     print("crc len: " + crc.length.toString());
@@ -50,21 +60,41 @@ class LyraCrypto {
   }
 
   /// Encode with checksum
-  Future<String> lyraEnc(String input) async {
-    final buff = hex.decode(input);
-    var crc = await checksum(buff);
-    return Base58.encode(buff + crc);
+  String lyraEnc(List<int> input) {
+    var crc = checksum(input);
+    return Base58.encode(input + crc);
   }
 
-  Future<List<int>> sha256(List<int> input) async {
-    final algo = Sha256();
-    final hash = await algo.hash(input);
-    return hash.bytes;
+  List<int> sha256(List<int> input) {
+    final algo = Digest("SHA-256");
+    final hash = algo.process(Uint8List.fromList(input));
+    return hash;
   }
 
-  Future<List<int>> checksum(List<int> input) async {
-    final h1 = await sha256(input);
-    var h2 = await sha256(h1);
+  List<int> checksum(List<int> input) {
+    final h1 = sha256(input);
+    var h2 = sha256(h1);
     return h2.sublist(0, 4);
+  }
+
+  String lyraEncPub(List<int> pubKeyBytes) {
+    var ret = lyraEnc(pubKeyBytes.sublist(1));
+    return "";
+  }
+
+  String prvToPub(String prvkey) {
+    var d = BigInt.parse(prvkey, radix: 16);
+    var curve = ECCurve_secp256r1();
+    var q = curve.G * d;
+    var pubKey = ECPublicKey(q, curve);
+    var pubKeyBytes = pubKey.Q.getEncoded(false);
+
+    return ""; // Base58() pubKeyBytes.sublist(1)
+  }
+
+  void GenerateWallet() {
+/*    final algo = Sha256();
+    final ec = Ecdsa.p256(algo);
+    var kp = await ec.newKeyPair(); */
   }
 }
--- test/lyra_crypto_test.dart
+++ test/lyra_crypto_test.dart
@@ -3,15 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lyra/lyra_crypto.dart';
 
 void main() {
-  test('key pair generate and verify', () async {
+  test('key pair generate and verify', () {
     String pvk = "2gbESTeBHsgt8um1aNN2dC9jajEDk3CoEupwmN6TRJQckyRbHa";
     String pub =
         "LUTkgGP9tb4iAFAFXv7i83N4GreEUakWbaDrUbUFnKHpPp46n9KF1QzCtvUwZRBCQz6yqerkWvvGXtCTkz4knzeKRmqid";
 
     final lyraCrypto = LyraCrypto();
     try {
-      var ret = await lyraCrypto.isAccountIdValid(pub);
+      var ret = lyraCrypto.isAccountIdValid(pub);
       expect(ret, true);
+
+      var ret2 = lyraCrypto.isPrivateKeyValid(pvk);
+      expect(ret2, true);
     } catch (e) {
       print(e);
       fail("not verify account id properly: ");
